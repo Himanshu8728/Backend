8c2b452e3a4b7243c8daddba536b6c67b8f80744
Himanshu Mourya <119477557+Himanshu8728@users.noreply.github.com>
2026-03-14 10:00:00 +0530

    Update backend

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -23,7 +23,8 @@ const generateAccessAndRefreshTokens = async (userId) => {
 
 const registerUser = asyncHandler(async (req, res) => {
     const { fullName, email, username, password } = req.body
-    //console.log("email:", email)
+    
+    // console.log("email:", email)
 
     if (
         [fullName, email, username, password].some((field) =>
